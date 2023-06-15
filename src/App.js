ad3f9757dc7f1694ddeddd22e8df34f8054446f6
RuquiaAlam <114290800+RuquiaAlam@users.noreply.github.com>
2023-06-15 11:49:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import { useSelector} from "react-redux";
+import { useSelector } from "react-redux";
 import Spinner from "./components/Spinner";
 import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
@@ -20,72 +20,72 @@ import DoctorAppointments from "./pages/doctor/DoctorAppointments";
 
 function App() {
 
-  const {loading} = useSelector(state => state.alerts);
+  const { loading } = useSelector(state => state.alerts);
   //target reducer
   return (
-  <>
-    {/* container */}
-<BrowserRouter>
-{loading ? (
-<Spinner/>
- ) : (
-<Routes>
-  <Route path ="/" element = {
-  <ProtectedRoute>
-    <HomePage />
-    </ProtectedRoute>}></Route>
+    <>
+      {/* container */}
+      <BrowserRouter>
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Routes>
+            <Route path="/" element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>}></Route>
 
-    <Route path ="/apply-doctor" element = {
-  <ProtectedRoute>
-    <ApplyDoctor/>
-    </ProtectedRoute>}></Route>
-    
-    <Route path ="/admin/users" element = {
-  <ProtectedRoute>
-    <Users />
-    </ProtectedRoute>}></Route>
+            <Route path="/apply-doctor" element={
+              <ProtectedRoute>
+                <ApplyDoctor />
+              </ProtectedRoute>}></Route>
 
-    <Route path ="/admin/doctors" element = {
-  <ProtectedRoute>
-    <Doctors />
-    </ProtectedRoute>}></Route>
-    <Route path ="/doctor/profile/:id" element = {
-  <ProtectedRoute>
-    <Profile />
-    </ProtectedRoute>}></Route>
-    <Route path ="/doctor/book-events/:doctorId" element = {
-  <ProtectedRoute>
-    <Events/>
-    </ProtectedRoute>}></Route>
-    
-    <Route path ="/notification" element = {
-  <ProtectedRoute>
-    <NotificationPage/>
-    </ProtectedRoute>}></Route>
-  <Route path="/login" element = {
-  <PublicRoute>
-  <Login/>
-  </PublicRoute>
-}></Route>
-  <Route path="/register" element= {
-  <PublicRoute>
-    <Register/>
-    </PublicRoute>}></Route>
+            <Route path="/admin/users" element={
+              <ProtectedRoute>
+                <Users />
+              </ProtectedRoute>}></Route>
 
-    <Route path="/doctor-appointments" element= {
-  <ProtectedRoute>
-    <DoctorAppointments />
-    </ProtectedRoute>}></Route>
-    <Route path="/appointments" element= {
-  <ProtectedRoute>
-    <Appointments/>
-    </ProtectedRoute>}></Route>
+            <Route path="/admin/doctors" element={
+              <ProtectedRoute>
+                <Doctors />
+              </ProtectedRoute>}></Route>
+            <Route path="/doctor/profile/:id" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>}></Route>
+            <Route path="/doctor/book-events/:doctorId" element={
+              <ProtectedRoute>
+                <Events />
+              </ProtectedRoute>}></Route>
 
-</Routes>
-)}
+            <Route path="/notification" element={
+              <ProtectedRoute>
+                <NotificationPage />
+              </ProtectedRoute>}></Route>
+            <Route path="/login" element={
+              <PublicRoute>
+                <Login />
+              </PublicRoute>
+            }></Route>
+            <Route path="/register" element={
+              <PublicRoute>
+                <Register />
+              </PublicRoute>}></Route>
 
-</BrowserRouter>
-  </>
+            <Route path="/doctor-appointments" element={
+              <ProtectedRoute>
+                <DoctorAppointments />
+              </ProtectedRoute>}></Route>
+            <Route path="/appointments" element={
+              <ProtectedRoute>
+                <Appointments />
+              </ProtectedRoute>}></Route>
+
+          </Routes>
+        )}
+
+      </BrowserRouter>
+    </>
   );
 }
 
